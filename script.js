2184df24271e23c3d906daf83fdcb353d8c1fe07
yagazierem <adaogbonna44@gmail.com>
2022-08-10 11:04:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Given your birthday and the current date, calculate your age in days.
+Compensate for leap days.
+Assume that the birthday and current date are correct dates (and no time travel). Simply put, if you were born 1Jan 2012 and today's date is 2 jan 2012, you're 1 day old.
+
